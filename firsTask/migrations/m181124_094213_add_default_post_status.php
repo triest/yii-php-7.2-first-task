f5751cd353b2b16cd63157bd4a0506914093eae5
triest <you@example.com>
2018-11-24 13:01:41 +0300

    add default value to past table

---
+++ firsTask/migrations/m181124_094213_add_default_post_status.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181124_094213_add_default_post_status
+ */
+class m181124_094213_add_default_post_status extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('post', 'status',$this->smallInteger()->defaultValue(1));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m181124_094213_add_default_post_status cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181124_094213_add_default_post_status cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
--- firsTask/modules/admin/controllers/PostController.php
+++ firsTask/modules/admin/controllers/PostController.php
@@ -93,7 +93,7 @@ class PostController extends Controller
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         }
-     
+
         return $this->render('update', [
             'model' => $model,
         ]);
